f120fb63090bdf2ca6d8a37a4d6c37ead0f3c4ee
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-08-18 18:34:48 +0800

    小程序demo

--- wechat/pages/index/index.js
+++ wechat/pages/index/index.js
@@ -1,6 +1,24 @@
 Page({
     data: {
-        current: 0
+        current: 0,
+        wxdemo: [
+            {
+                demo: 'scroll-view',
+                index: 0
+            },
+            {
+                demo: 'form',
+                index: 1
+            },
+            {
+                demo: 'map',
+                index: 2
+            },
+            {
+                demo: 'template',
+                index: 3
+            }
+        ]
     },
     onLoad(options) {
 
@@ -11,22 +29,47 @@ Page({
     onShow() {
 
     },
-    swiper(event){
-        console.log(event)
+    swiper(event) {
+        //console.log(event)
     },
-    tabsChange(event){
-        console.log(event)
+    tabsChange(event) {
+        //console.log(event)
         var current = event.detail.current;
         this.setData({
             current
         })
     },
-    tabTap(event){
-        console.log(event.currentTarget.dataset.current)
+    tabTap(event) {
+        //console.log(event.currentTarget.dataset.current)
         var current = event.currentTarget.dataset.current;
         this.setData({
             current
         })
+    },
+    swiperTap(event) {
+        var swiperIndex = event.currentTarget.dataset.swiperIndex;
+        console.log(swiperIndex)
+        switch (swiperIndex) {
+            case 0:
+                wx.navigateTo({
+                    url: '../scroll/scroll',
+                });
+                break;
+            case 1:
+                wx.navigateTo({
+                    url: '../form/form',
+                });
+                break;
+            case 2:
+                wx.navigateTo({
+                    url: '../map/map',
+                });
+                break;
+            case 3:
+                wx.navigateTo({
+                    url: '../template/template',
+                })
+        }
     },
     onPullDownRefresh() {
 
