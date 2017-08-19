f9bcdf99667bdf0a071e2b085bfdae1de0d07e53
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-08-19 09:32:58 +0800

    小程序demo

--- wechat/pages/template/template.js
+++ wechat/pages/template/template.js
@@ -1,12 +1,23 @@
 Page({
     data:{
-        common:'commonTemplate'
+        common:[
+            {
+                code:0,
+                name:'zhangsan'
+            },{
+                code:1,
+                name:'lisi'
+            }
+        ]
     },
     onLoad(){
 
     },
     onShow(){
 
+    },
+    templateTap(event){
+        console.log(event)
     },
     onReady(){
 
