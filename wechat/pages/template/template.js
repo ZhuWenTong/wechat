d8ba97c0e22b1b7f071974a849c270163e4b1005
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-08-29 13:09:21 +0800

    小程序demo

--- wechat/pages/template/template.js
+++ wechat/pages/template/template.js
@@ -8,6 +8,53 @@ Page({
                 code:1,
                 name:'lisi'
             }
+        ],
+        iconTypes: [
+            {
+                type:'success',
+                color:'',
+                size:16
+            },
+            {
+                type: 'success_no_circle',
+                color: '',
+                size: 17
+            },
+            {
+                type: 'info',
+                color: '',
+                size: 18
+            },
+            {
+                type: 'warn',
+                color: '',
+                size: 19
+            },
+            {
+                type: 'waiting',
+                color: '',
+                size: 20
+            },
+            {
+                type: 'cancel',
+                color: '',
+                size: 21
+            },
+            {
+                type: 'download',
+                color: '',
+                size: 22
+            },
+            {
+                type: 'search',
+                color: '',
+                size: 23
+            },
+            {
+                type: 'clear',
+                color: '',
+                size: 24
+            }
         ]
     },
     onLoad(){
