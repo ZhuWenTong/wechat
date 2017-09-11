465e85cef062236eaccfd6c52cf80f9d85ac8f23
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-09-11 16:34:38 +0800

    小程序demo

--- wechat/pages/map/map.js
+++ wechat/pages/map/map.js
@@ -9,7 +9,22 @@ Page({
 
     },
     onShow() {
-
+        wx.getLocation({
+            success: (res) => {
+                console.log(res)
+                // this.setData({
+                //     latitude: Number(res.latitude),
+                //     longitude: Number(res.longitude)
+                // });
+                wx.openLocation({
+                    latitude: Number(res.latitude),
+                    longitude: Number(res.longitude),
+                })
+            },
+            fail(err){
+                console.log(err)
+            }
+        })
     },
     onUnload() {
 
