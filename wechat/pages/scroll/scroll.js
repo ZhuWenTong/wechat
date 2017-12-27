39111f073e08aa86cb5a3cdf1a330f4f5b0fef4e
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2017-12-27 12:00:11 +0800

    wechat demo

--- wechat/pages/scroll/scroll.js
+++ wechat/pages/scroll/scroll.js
@@ -21,6 +21,17 @@ Page({
     toBottom() {
         console.log('滚动到底部')
     },
+    containerTap: function (res) {
+        console.log(res.touches[0]);
+        var x = res.touches[0].pageX;
+        var y = res.touches[0].pageY + 85;
+        this.setData({
+            rippleStyle: ''
+        });
+        this.setData({
+            rippleStyle: `top: ${y}px;left: ${x}px;-webkit-animation: ripple 0.4s linear;animation:ripple 0.4s linear;`
+        });
+    },
     onReady() {
 
     },
