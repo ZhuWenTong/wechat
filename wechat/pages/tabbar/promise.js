fdd99aac1f1ca42911cadd206d11abcf3253dc01
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2018-01-03 16:24:05 +0800

    小程序 demo

--- wechat/pages/tabbar/promise.js
+++ wechat/pages/tabbar/promise.js
@@ -4,11 +4,12 @@ export function add(obj) {
         obj.num += 10;
         //console.log(obj);
         obj.self.setData({
-            val: obj.num
+            val: obj.num,
+            tit: `${obj.self.data.val} + 10 = `
         })
         setTimeout(() => {
             resolve(obj);
-        }, 500)
+        }, 2000)
     })
 }
 
@@ -19,10 +20,56 @@ export function min(obj) {
         //console.log(obj)
         obj.self.setData({
             val: obj.num,
-            loading: false
+            tit: `${obj.self.data.val} - 5 = `
         })
         setTimeout(() => {
-            resolve(obj.num);
-        }, 500)
+            resolve(obj);
+        }, 2000)
+    })
+}
+
+export function multiple(obj) {
+    return new Promise((resolve, reject) => {
+        obj.num *= 10;
+        obj.self.setData({
+            val: obj.num,
+            tit: `${obj.self.data.val} * 10 = `
+        })
+        setTimeout(() => {
+            resolve(obj);
+        }, 2000);
+    })
+}
+
+export function divsion(obj) {
+    return new Promise((resolve, reject) => {
+        obj.num /= 5;
+        obj.self.setData({
+            val: obj.num,
+            tit: `${obj.self.data.val} / 5 = `
+        });
+        setTimeout(() => {
+            resolve(obj);
+        }, 2000);
+    })
+}
+
+export function finish(obj) {
+    return new Promise((resolve, reject) => {
+        obj.self.setData({
+            tit: '结果为: '
+        });
+        setTimeout(() => {
+            resolve(obj);
+        }, 2000)
+    })
+}
+
+export function beAgain(obj) {
+    return new Promise((resolve, reject) => {
+        obj.self.setData({
+            tit: '点击按钮开始',
+            loading: false
+        })
     })
 }
--- wechat/pages/tabbar/tabbar.js
+++ wechat/pages/tabbar/tabbar.js
@@ -1,13 +1,26 @@
-import {add, min} from 'promise.js';
+import {add, min, multiple, divsion, finish, beAgain} from 'promise.js';
 Page({
     data: {
+        val: 10,
+        tit: '点击按钮开始',
         loading: false
     },
     onLoad() {
 
     },
     onShow() {
-
+        var p1 = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('p1');
+            }, 1000);
+        }),
+        p2 = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('p2');
+            }, 2000);
+        });
+        Promise.all([p1, p2]).then(result => console.log(result));
+        Promise.race([p1, p2]).then(result => console.log(result))
     },
     /**
      * @date 2018-01-03
@@ -22,7 +35,7 @@ Page({
         var p = new Promise((resolve, reject) => {
             resolve(obj);
         })
-        p.then(add).then(min).catch(err => console.log(err));
+        p.then(add).then(min).then(multiple).then(divsion).then(finish).then(beAgain).catch(err => console.log(err));
         this.setData({
             loading: true
         })
