199702c253fa782a00760b9eb909b04b9a6b213b
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2018-07-30 10:53:24 +0800

    新增使用iview组件

---
+++ wechat/pages/iview/iview.js
@@ -0,0 +1,17 @@
+Page({
+    data: {
+        current: 0
+    },
+    onLoad() {
+
+    },
+    onShow() {
+
+    },
+    handleClick() {
+        const current = this.data.current + 1 > 2 ? 0 : this.data.current + 1;
+        this.setData({
+            current
+        })
+    }
+})
