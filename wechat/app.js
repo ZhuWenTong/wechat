c60839c09ef0350edb3a14f9be314344db9958ff
zwt <30827013+ZhuWenTong@users.noreply.github.com>
2018-08-30 11:00:51 +0800

    wx.getUserInfo

--- wechat/app.js
+++ wechat/app.js
@@ -8,6 +8,23 @@ App({
                 //console.log(res)
                 this.systemInfo = res;
             },
+        });
+        wx.getSetting({
+            success: res => {
+                if(res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            this.globalData.userInfo = res.userInfo;
+                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                            // 所以此处加入 callback 以防止这种情况
+                            if(this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res);
+                            }
+                        }
+                    })
+                }
+            }
         })
     },
     onShow(options) {
@@ -15,6 +32,9 @@ App({
     },
     onHide() {
 
+    },
+    globalData: {
+        userInfo: null
     },
     onError(msg) {
 
